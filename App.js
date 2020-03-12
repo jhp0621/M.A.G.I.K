3a767c70a88e8b4234ca5922cd1dc7451b1b104c
Ji Hye Park <jhp0621@gmail.com>
2020-03-12 11:30:02 -0500

    testing

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import Greeting from './components/Greeting'
+
+let pic = {uri: 'https://noma.org/wp-content/uploads/2018/12/mindfulness.jpg'}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Mindfulness app</Text>
+      <Text style={styles.title}>Mindfulness app</Text>
+      <Greeting name="yogi" />
+      <Image source={pic} style={{width: 400, height: 500}}/>
     </View>
   );
 }
@@ -16,4 +21,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 30,
+  }
 });
